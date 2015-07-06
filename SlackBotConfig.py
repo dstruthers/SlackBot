5984c60de8a4757dbe520a3c1b68e177a927fe0f
Darren Struthers <dstruthers@LIBR17962.plu.edu>
2015-07-06 14:26:08 -0700

    SlackBotConfig now looks for SlackBot.cfg in its own directory, regardless of what the current working directory is.

--- SlackBotConfig.py
+++ SlackBotConfig.py
@@ -1,7 +1,8 @@
-import ConfigParser
+import ConfigParser, os
 
 config = ConfigParser.RawConfigParser()
-config.read('SlackBot.cfg')
+config_dir = os.path.dirname(os.path.realpath(__file__))
+config.read(config_dir + '/SlackBot.cfg')
 
 SLACK_API_BASE = config.get('General', 'api_base_uri')
 DEBUG = config.getboolean('General', 'debug')
